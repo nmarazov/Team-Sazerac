d2eb761fa87c3a1a57781c879ce81a8d9578cc6c
AnnaYanakieva <a.ianakieva@gmail.com>
2017-01-11 09:09:35 +0200

    Added ManualPlayer.cs

---
+++ OOPTeamwork/OOPTeamwork/Models/Players/ManualPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using OOPTeamwork.Core;
+using OOPTeamwork.Models.GameAbstracts;
+
+namespace OOPTeamwork.Models.Players
+{
+    public class ManualPlayer : Player
+    {
+        public ManualPlayer(int playerIndex, char playerSymbol) : base(playerIndex, playerSymbol)
+        {
+        }
+
+        public override void PlayerNextMove()
+        {
+            Console.Write($"Player {this.PlayerIndex} select position: ");
+
+            var position = int.Parse(Console.ReadLine());
+
+            if (GameLogic.CheckIfPositionIsTaken(position) == true)
+            {
+                this.PlayerNextMove();
+            }
+            else
+            {
+                GameField.Instance.InputSelection[position - 1] = this.PlayerSymbol;
+            }
+        }
+    }
+}
