29045964d53db989f709f2b321b561ab14e3bc14
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-01-14 18:46:18 +0200

    Update Result.cs

--- OOPTeamwork/OOPTeamwork/Core/Result.cs
+++ OOPTeamwork/OOPTeamwork/Core/Result.cs
@@ -1,6 +1,47 @@
-﻿namespace OOPTeamwork.Core
+using System;
+
+namespace OOPTeamwork.Core
 {
     public class Result
     {
+        private int playerOneNumOfWins;
+        private int playerTwoNumOfWins;
+
+        public int PlayerOneNumOfWins
+        {
+            get { return playerOneNumOfWins; }
+            private set { playerOneNumOfWins = value; }
+        }
+        public int PlayerTwoNumOfWins
+        {
+            get { return playerTwoNumOfWins; }
+            private set { playerOneNumOfWins = value; }
+        }
+
+        public void PlayerWin(int playerNum)
+        {
+            if (playerNum == 1)
+            {
+                playerOneNumOfWins++;
+            }
+            if (playerNum == 2)
+            {
+                playerTwoNumOfWins++;
+            }
+        }
+        public void PrintResult()
+        {
+            Console.WriteLine("The result is:");
+            Console.ForegroundColor = System.ConsoleColor.Red;
+            Console.WriteLine(string.Format("Player 1: {0} wins", PlayerOneNumOfWins));
+            Console.ForegroundColor = System.ConsoleColor.Blue;
+            Console.WriteLine(string.Format("Player 2: {0} wins", PlayerTwoNumOfWins));
+            Console.ForegroundColor = default(System.ConsoleColor);
+        }
+        public void ClearResult()
+        {
+            this.PlayerOneNumOfWins = 0;
+            this.PlayerTwoNumOfWins = 0;
+        }
     }
 }
