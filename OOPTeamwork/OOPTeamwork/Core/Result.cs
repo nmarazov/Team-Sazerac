33abb512316fba3a563f329498ca8f7c8496c459
AngelK99 <f3x5@mail.bg>
2017-01-15 17:45:35 +0200

    Update Result.cs

--- OOPTeamwork/OOPTeamwork/Core/Result.cs
+++ OOPTeamwork/OOPTeamwork/Core/Result.cs
@@ -1,71 +1,37 @@
 using System;
-using System.Text;
 
 namespace OOPTeamwork.Core
 {
-    public class Result
+    public static class Result
     {
-        private static Result instance;
+        private static int playerOneNumOfWins;
+        private static int playerTwoNumOfWins;
 
-        private int playerOneNumOfWins;
-        private int playerTwoNumOfWins;
-
-        private Result()
-        {  
-        }
-
-        public static Result Instance
+        public static void PlayerWin(int playerNum)
         {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Result();
-                }
-
-                return instance;
-            }
-        }
-
-        public int PlayerOneNumOfWins
-        {
-            get { return playerOneNumOfWins; }
-            private set { playerOneNumOfWins = value; }
-        }
-
-        public int PlayerTwoNumOfWins
-        {
-            get { return playerTwoNumOfWins; }
-            private set { playerTwoNumOfWins = value; }
-        }
-
-        public void PlayerWin(int playerNum)
-        {
-            if (playerNum % 2 == 1)
+            if (playerNum == 1)
             {
                 playerOneNumOfWins++;
             }
-            if (playerNum % 2 == 0)
+            if (playerNum == 2)
             {
                 playerTwoNumOfWins++;
             }
         }
 
-        public string PrintResult()
+        public static void PrintResult()
         {
-            var result  = new StringBuilder();
-
-            result.AppendLine("The result is:");
-            result.AppendLine($"Red player: {PlayerOneNumOfWins} wins");
-            result.AppendLine($"Blue player: {PlayerTwoNumOfWins} wins");
-
-            return result.ToString();
+            Console.WriteLine("The result is:");
+            Console.ForegroundColor = System.ConsoleColor.Red;
+            Console.WriteLine("Player 1: {0} wins", playerOneNumOfWins);
+            Console.ForegroundColor = System.ConsoleColor.Blue;
+            Console.WriteLine("Player 2: {0} wins", playerTwoNumOfWins);
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
-
-        public void ClearResult()
+        public static void ClearResult()
         {
-            this.PlayerOneNumOfWins = 0;
-            this.PlayerTwoNumOfWins = 0;
+            playerOneNumOfWins = 0;
+            playerTwoNumOfWins = 0;
         }
     }
 }
